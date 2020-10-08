d8eddf6756a4a6110b239ec9368843ed0ec35ae0
cieloginzafarm <cielo@ginzafarm.co.jp>
2020-10-08 16:40:16 +0900

    Add some markers for each prefecture

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import * as covidData from "./data/japan-covid.json";
+import "./App.css";
 
 function App() {
+  const [viewport, setViewPort] = useState({
+    latitude: 36,
+    longitude: 137,
+    width: "100vw",
+    height: "100vh",
+    zoom: 4.5,
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/cieloginzafarm/ckg0gyibf04ld1aqds8wen9jh"
+        onViewportChange={(viewport) => {
+          setViewPort(viewport);
+        }}
+      >
+        {covidData.area.map((prefecture) => (
+          <Marker
+            key={prefecture.name}
+            latitude={prefecture.coordinates[0]}
+            longitude={prefecture.coordinates[1]}
+          >
+            <button class="marker-btn">
+              <img src="/covid.png" alt="Japan Covid Icon"/>
+            </button>
+          </Marker>
+        ))}
+      </ReactMapGL>
     </div>
   );
 }
