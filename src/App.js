5b54197db29bba9a5333ecd42ab858ed8fe1ebaa
cieloginzafarm <cielo@ginzafarm.co.jp>
2020-10-08 17:18:44 +0900

    Added some Popups and listeners.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import * as covidData from "./data/japan-covid.json";
 import "./App.css";
 
@@ -11,6 +11,27 @@ function App() {
     height: "100vh",
     zoom: 4.5,
   });
+
+  const [selectedPrefecture, setSelectedPrefecture] = useState(null);
+
+  useEffect(() => {
+    const keyListener = (e) => {
+      if (e.key === "Escape") {
+      setSelectedPrefecture(null);
+    }};
+
+    const mouseListener = () => {
+        setSelectedPrefecture(null);
+    };
+    
+    window.addEventListener("keydown", keyListener);
+    window.addEventListener("mousedown", mouseListener);
+
+    return () => {
+      window.removeEventListener("keydown", keyListener);
+      window.removeEventListener("mousedown", mouseListener);
+    }
+  }, [])
   return (
     <div className="App">
       <ReactMapGL
@@ -27,11 +48,37 @@ function App() {
             latitude={prefecture.coordinates[0]}
             longitude={prefecture.coordinates[1]}
           >
-            <button class="marker-btn">
-              <img src="/covid.png" alt="Japan Covid Icon"/>
+            <button
+              className="marker-btn"
+              onClick={(e) => {
+                e.preventDefault();
+                setSelectedPrefecture(prefecture);
+              }}
+            >
+              <img src="/covid.png" alt="Japan Covid Icon" />
             </button>
           </Marker>
         ))}
+        {selectedPrefecture && (
+          <Popup
+            latitude={selectedPrefecture.coordinates[0]}
+            longitude={selectedPrefecture.coordinates[1]}
+            onClose={() => {
+              setSelectedPrefecture(null);
+            }}
+          >
+            <div>
+              <h2>{selectedPrefecture.name}</h2>
+              <p>Total Number of Patients: {selectedPrefecture.npatients}</p>
+              <p>
+                Number of Current Patients:{" "}
+                {selectedPrefecture.ncurrentpatients}
+              </p>
+              <p>Number of Discharged Patients: {selectedPrefecture.nexits}</p>
+              <p>Number of Deaths: {selectedPrefecture.ndeaths}</p>
+            </div>
+          </Popup>
+        )}
       </ReactMapGL>
     </div>
   );
